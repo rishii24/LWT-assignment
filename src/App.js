0bfb0bdc599e04e08a17e23aef7994a73176c6a7
rishii24 <24rishi02@gmail.com>
2022-09-03 23:46:50 +0530

    routing setup and comp integration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+import Search from "./pages/Search";
+import Favourites from "./pages/Favourites";
+import Stats from "./pages/Stats";
+
+const App = () => {
   return (
     <div className="App">
-     HI
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/search" />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/fav" element={<Favourites />} />
+          <Route path="/stats" element={<Stats />} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
