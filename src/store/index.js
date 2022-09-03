9f713812f5f2012b9732db2183257499bf0029ed
rishii24 <24rishi02@gmail.com>
2022-09-03 23:50:12 +0530

    reducer setup for store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import receiverReducers from "../features/receiver/api";
+import videoReducers from "../api";
 export default configureStore({
-//   reducer: { ...receiverReducers },
+  reducer: { ...videoReducers },
 });
