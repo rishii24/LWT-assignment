25a3f7576fbb2904adef52c506515e519cf8a152
rishii24 <24rishi02@gmail.com>
2022-09-03 23:48:23 +0530

    navigation bar

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Img from "../assets/youtube.webp";
+import SearchBar from "./SearchBar";
+
+const Navbar = () => {
+  return (
+    <div className="">
+      <div className="shadow-md flex items-center justify-around px-4 py-4 text-xl">
+        <div>
+          <img className="h-12" src={Img} alt="logo" />
+        </div>
+        <div className="w-1/3">
+          <SearchBar />
+        </div>
+        <div className=" w-1/3 flex justify-around items-center">
+          <NavLink className="mx-4 pb-1 hidden md:inline-block" to="/search">
+            Home
+          </NavLink>
+          <NavLink className="mx-4 pb-1 hidden md:inline-block" to="/fav">
+            Favourites
+          </NavLink>
+          <NavLink className="mx-4 pb-1 hidden md:inline-block" to="/stats">
+            Stats
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
