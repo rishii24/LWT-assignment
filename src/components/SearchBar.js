845edcc5b8aac3a5bf672f2d7ed4740079eb5467
rishii24 <24rishi02@gmail.com>
2022-09-03 23:48:44 +0530

    search bar comp

---
+++ src/components/SearchBar.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userSearchThunk } from "../api/index";
+
+import Navbar from "../components/Navbar";
+
+const SearchBar = () => {
+  const dispatch = useDispatch();
+  const [searchInput, setSearchInput] = useState("");
+
+  const onChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const clickHandler = async (e) => {
+    e.preventDefault();
+    dispatch(userSearchThunk(searchInput));
+  };
+
+  return (
+    <div className="w-full">
+      <form onSubmit={clickHandler} className="flex mx-auto">
+        {" "}
+        <input
+          onChange={onChange}
+          className="flex-1 appearance-none border border-secondary-400 rounded md:rounded-sm w-full py-2 px-4 text-sm outline-none"
+          type={"text"}
+          value={searchInput}
+          required={true}
+          placeholder="Search here.."
+        />
+        <button
+          onClick={clickHandler}
+          className="flex items-center px-3 py-1 cursor-pointer bg-gray-500 w-max text-white text-sm"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;
