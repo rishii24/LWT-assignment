7c947a8302b84c1a9ab69812d1eb6dc014669910
rishii24 <24rishi02@gmail.com>
2022-09-04 23:36:00 +0530

    fav video comp

---
+++ src/components/FavCard.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import FavVideoModal from "./FavVideoModal";
+import { useDispatch } from "react-redux";
+import { userUnfavThunk } from "../api";
+
+const FavCard = ({ data }) => {
+
+  const dispatch = useDispatch();
+  const [modal, setModal] = useState(false);
+
+  const handleUnfav = (data) => {
+    dispatch(userUnfavThunk(data));
+  };
+
+  return (
+    <div className="flex flex-col p-4 border border-blue-100">
+      <div className="cursor-pointer" onClick={() => setModal(!modal)}>
+        <img
+          className="lg:h-44 md:h-36 h-32 w-full"
+          src={data.data.snippet.thumbnails.high.url}
+          alt="thibnail"
+        />
+      </div>
+      <div className="flex flex-col mt-8">
+        <div className="flex justify-between lg:text-sm md:text-xs my-2 font-medium">
+          <div>{data.data.snippet.channelTitle}</div>{" "}
+          <button onClick={() => handleUnfav(data)}>
+            Remove
+          </button>
+        </div>
+        <div className="lg:text-lg md:text-md font-medium h-24">
+          {data.data.snippet.title}
+        </div>
+        <div className="text-sm mt-4">{data.data.snippet.description}</div>
+      </div>
+
+      {modal && <FavVideoModal data={data} setModal={setModal} />}
+    </div>
+  );
+};
+
+export default FavCard;
