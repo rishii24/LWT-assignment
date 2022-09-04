4d2b50130ec56a04e6da18e85bf1a9a85c3d0410
rishii24 <24rishi02@gmail.com>
2022-09-04 23:36:14 +0530

    fav video playback modal

---
+++ src/components/FavVideoModal.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const FavVideoModal = ({ data,setModal }) => {
+  const videoSrc = `https://www.youtube.com/embed/${data.data.id.videoId}`;
+
+  return (
+    <div className="flex">
+      <div className="flex justify-center items-center h-3/4 fixed left-16 md:left-28 lg:left-64 top-12 bg-black z-50 w-2/3">
+      <button
+          className="flex absolute top-0 right-0 px-2 py-1 bg-green-400 text-white text-xs"
+          onClick={() => setModal(false)}
+        >
+          exit
+        </button>
+        <iframe
+          height="380"
+          width="750"
+          src={videoSrc}
+          allowFullScreen
+          title="Video player"
+        />
+      </div>
+      <div className="fixed h-screen w-full top-0 left-0 bg-black opacity-50"></div>
+    </div>
+  );
+};
+
+export default FavVideoModal;
