f5bcce2b82e4ded46866bdbfa9d3d2046422c5be
rishii24 <24rishi02@gmail.com>
2022-09-04 23:42:58 +0530

    ui changes

--- src/components/ChannelCard.js
+++ src/components/ChannelCard.js
@@ -6,14 +6,14 @@ const ChannelCard = ({ data }) => {
     <div className="flex my-8 items-center px-4 py-4 border border-blue-400">
       <div className="flex items-center md:h-40 md:w-40 mx-4">
         <img
-          className="rounded-full md:h-40 md:w-40"
+          className="rounded-full lg:h-40 lg:w-40"
           src={data.snippet.thumbnails.high.url}
           alt="Channel logo"
         />
       </div>
       <div className="flex flex-col ml-8">
-        <div className="text-2xl font-medium">{data.snippet.channelTitle}</div>
-        <div>{data.snippet.description}</div>
+        <div className="lg:text-2xl text-lg font-medium">{data.snippet.channelTitle}</div>
+        <div className="lg:text-lg text-xs">{data.snippet.description}</div>
       </div>
     </div>
   );
