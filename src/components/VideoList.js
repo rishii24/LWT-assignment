a3ef5f95f9845290588eb4dba8db52ac023e95f4
rishii24 <24rishi02@gmail.com>
2022-09-04 23:43:39 +0530

    ui fixed

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -11,17 +11,16 @@ const VideoList = () => {
     setChannelData(listData?.items);
   }, [listData]);
 
-  console.log(channelData, "video list");
   return (
     <div className="flex flex-col md:px-20 px-8">
       {channelData &&
         channelData.map((data, index) => {
           if (index === 0) return <ChannelCard data={data} />;
         })}
-      <div className="grid grid-rows-4 grid-cols-4 gap-4">
+      <div className="grid grid-rows-4 lg:grid-cols-4 md:grid-cols-3 grid-col-2 gap-4">
         {channelData &&
           channelData.map((data, index) => {
-            if (index !== 0) return <VideoCard data={data} />;
+            if (index !== 0) return <VideoCard data={data} key={data.etag} />;
           })}
       </div>
     </div>
